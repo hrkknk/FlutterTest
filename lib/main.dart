83ba9074ee6426af9ff647657ac8e7f6b8af9ea9
hiroki kaneko <hrkknk@hirokinoMac-mini.local>
2023-10-31 17:07:42 +0900

    軽微修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/firebase_options.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:firebase_core/firebase_core.dart';
 // import 'dart:convert';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     const MaterialApp(
       home: WebViewApp(),
